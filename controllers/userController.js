9dcf783c0e25a6855014896fbb08c60286a66e50
LEAKONO <leakonoemmanuel3@gmail.com>
2025-06-03 23:05:03 +0300

    feat: implement CRUD operations for User model

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,10 @@ exports .getUsers=async(req,res)=>{
 }
 exports.getUser=async(req,res)=>{
     try{
-        const user=await User.findOne(req.params.id)
+        const user=await User.findById(req.params.id)
+        if(!user) return res.status(404).json({
+            error:"user not found"
+        })
         res.status(200).json(user)
     }catch(err){
         res.status(500).json({error:err.message})
@@ -27,16 +30,20 @@ exports.getUser=async(req,res)=>{
 }
 exports.updateUser=async(req,res)=>{
     try{
-    const updateOne=await User.findByIdAndUpdate(req.params.id,req.body,{new:true})
-    res.status(200).json(updateOne)
+    const updatedUser=await User.findByIdAndUpdate(req.params.id,req.body,{new:true})
+    if (!updatedUser) return res.status(404).json({ error: "User not found" });
+
+    res.status(200).json(updatedUser)
     }catch(err){
         res.status(500).json({error:err.message})
     }
 }
 exports.deleteUser=async(req,res)=>{
     try{
-const deleted=await User.findByIdAndDelete(req.params.id)
-res.status(200).json(deleted)
+const deletedUser=await User.findByIdAndDelete(req.params.id)
+ if (!deletedUser) return res.status(404).json({ error: "User not found" });
+
+res.status(200).json(deletedUser)
     }catch(err){
         res.status(500).json({error:err.message})
     }
